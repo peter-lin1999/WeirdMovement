40f3bf64529ec9801e2b2a8512ab0cc426d89d9f
peter-lin1999 <52004680+peter-lin1999@users.noreply.github.com>
2022-09-04 09:18:30 +0800

    peter修改
    新增menuscene、load其他Scene的Buildsetting、

---
+++ Assets/Scripts/Mainmenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
